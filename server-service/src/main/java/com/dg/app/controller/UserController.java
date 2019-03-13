fb9b13948e065691f232d02a55797828eb769f93
hazel <ha.ferraris@gmail.com>
2019-03-13 15:59:03 +0800

    added logger, used log4j2 with slf4j - yml config and xml config

--- server-service/src/main/java/com/dg/app/controller/UserController.java
+++ server-service/src/main/java/com/dg/app/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.dg.app.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-
+    public static Logger logger = LoggerFactory.getLogger(UserController.class);
     @GetMapping("/")
     public String greetings(){
         return "hello";
@@ -14,6 +16,7 @@ public class UserController {
 
     @GetMapping("/person/{username}")
     public String person(@PathVariable String username){
+        logger.info("Person : {}", username);
         return "hello "+ username;
     }
 }
--- server-service/src/main/java/com/dg/app/main/ServiceMain.java
+++ server-service/src/main/java/com/dg/app/main/ServiceMain.java
@@ -1,6 +1,8 @@
 package com.dg.app.main;
 
 import com.dg.app.controller.UserController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +18,15 @@ import org.springframework.context.annotation.Import;
 @Import(UserController.class)
 public class ServiceMain
 {
+    public static Logger logger = LoggerFactory.getLogger(ServiceMain.class);
     public static void main (String[] args){
         SpringApplication.run(ServiceMain.class, args);
     }
 
     @Bean
     public CommandLineRunner cmdLineRunner(ApplicationContext ctx){
-        return args -> System.out.println("ctx.getApplicationName() = " + ctx.getDisplayName().toString());
+        logger.info("Service test : geon");
+        return args -> System.out.println("ctx.getApplicationName() = " + ctx.getDisplayName());
 
     }
 }
